d7b4ccb88c2a3b7d1b65caf4d701e70686bf8db2
hadi493 <hadialam493@gmail.com>
2024-04-28 14:52:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+    fs.readdir(`./files`, function(err, files) {
+        res.render('index', {files: files});
+    })
+});
+
+app.post('/create', function(req, res) {
+    fs.writeFile(`./files/${req.body.title.split(" ").join('')}.txt`, req.body.detail, function(err) {
+        res.redirect('/');
+    });
+});
+
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, filedata) => {
+        res.render('show', {filename: req.params.filename, filedata: filedata});
+    });
+});
+
+app.get('/edit/:filename', (req, res) => {
+    res.render('edit', {filename: req.params.filename});
+});
+
+app.post('/edit', (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err) => {
+        res.redirect('/');
+    });
+});
+
+app.listen(3000);
